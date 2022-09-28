f18f0237da293e154b4783648a8cf69e9c1dc986
Kyanh Pham <kyanh.pham@mths.ca>
2022-09-28 15:23:35 +0000

    Calculates the circumference of circle with user input

---
+++ circumferenceOfCircle.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Kyanh Pham
+//
+// Created by: Kyanh Pham
+// Created on: Sept 2022
+// This program calculates the circumference of a circle
+// with user input
+
+#include <iostream>
+
+int main() {
+    // this function calculates circumference of a circle
+    const double TAU = 6.28;
+    int radius;
+    double circumference;
+
+    // input
+    std::cout << "Enter radius of the circle (mm): ";
+    std::cin >> radius;
+
+    // process
+    circumference = TAU * radius;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "Circumference is " << circumference << " mm" << std::endl;
+    std::cout << "\nDone." << std::endl;
+}
